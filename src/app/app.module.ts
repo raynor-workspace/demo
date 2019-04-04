ef6eda422319c28d58ad9de8d1b02f244911a916
raynor-workspace <49219985+raynor-workspace@users.noreply.github.com>
2019-04-04 11:56:39 +0800

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,68 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {
+  BrowserModule,
+  BrowserTransferStateModule,
+} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import {
+  StorefrontComponent,
+  StorefrontModule,
+  translations,
+  defaultCmsContentConfig,
+} from '@spartacus/storefront';
+
+import { environment } from '../environments/environment';
+import { ConfigModule } from '@spartacus/core';
+
+const devImports = [];
+
+if (!environment.production) {
+  devImports.push(StoreDevtoolsModule.instrument());
+}
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'spartacus-app' }),
+    BrowserTransferStateModule,
+    StorefrontModule.withConfig({
+      production: environment.production,
+      server: {
+        baseUrl: environment.occBaseUrl,
+      },
+      pwa: {
+        enabled: true,
+        addToHomeScreen: true,
+      },
+      siteContext: {
+        urlEncodingParameters: ['BASE_SITE', 'LANGUAGE', 'CURRENCY'],
+        parameters: {
+          BASE_SITE: {
+            values: ['electronics-spa', 'apparel-de', 'apparel-uk'],
+            defaultValue: 'electronics-spa',
+            persistence: 'route',
+          },
+        },
+      },
+      routesConfig: {
+        translations: {
+          default: {
+            product: {
+              paths: ['product/:productCode', 'product/:name/:productCode'],
+            },
+          },
+        },
+      },
+      i18n: {
+        resources: translations,
+      },
+    }),
+
+    ConfigModule.withConfigFactory(defaultCmsContentConfig),
+    ...devImports,
+  ],
+
+  bootstrap: [StorefrontComponent],
 })
-export class AppModule { }
+export class AppModule {}
